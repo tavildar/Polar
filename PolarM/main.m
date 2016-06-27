16e9a468b11a553e2e17c6b4d7a9474e6aeb8887
tavildar <tavildar@gmail.com>
2016-06-27 10:15:46 -0400

    Update main.m

--- PolarM/main.m
+++ PolarM/main.m
@@ -3,11 +3,11 @@
 clear all;
 n = 11;
 block_length = 2^n;
-info_length = 2^(n-2);
+info_length = 2^(n-1);
 crc_size = 0;
 epsilon = 0.32;
 list_vec = [1, 4];
-ebno_vec = 1:0.25:2;
+ebno_vec = 1:0.5:3;
 polar_code = PolarCode(block_length, info_length, epsilon, crc_size);
 [bler, ~] = polar_code.get_bler_quick(ebno_vec, list_vec);
 [ebno_vec', bler]
